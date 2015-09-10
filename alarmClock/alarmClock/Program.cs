0dbd46eec9f827f0a5c1c055acf4adc44d7e8c7f
rlingineni <Raviteja@Ravitejas-MacBook-Pro.local>
2015-09-10 11:24:16 -0500

    added solution

---
+++ alarmClock/alarmClock/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Media;
+using System.Timers;
+
+namespace alarmClock
+{
+	class MainClass
+	{
+		static Timer timer;
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("How many seconds do you want me to beep?");
+			string seconds = Console.ReadLine ();
+			int secs;
+
+			if (int.TryParse(seconds, out secs)) {
+				secs*=1000;
+				// Create a timer with a two second interval.
+				timer = new System.Timers.Timer (secs);
+
+				// Hook up the Elapsed event for the timer. 
+				timer.Elapsed += OnTimedEvent;
+
+				// Start the timer
+				timer.Enabled = true;
+
+				//Garbage Collector keep timer alive for eternity 
+				GC.KeepAlive (timer);
+
+				Console.WriteLine ("Press the Enter key to exit the program at any time... ");
+				Console.ReadLine ();
+			}
+			else
+			{
+
+				Console.WriteLine ("Uh-Oh, enter a valid integer");
+			}
+
+		}
+
+		private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+		{
+			Console.Beep ();
+		}
+	}
+}
